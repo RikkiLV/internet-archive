fe2a06c1df84ebbfc2f56c2387a6c978fd0147f8
Rikki Lee <106826637+RikkiLV@users.noreply.github.com>
2025-04-23 17:43:38 +0000

    Adds comments for database scripts. Creates scraper for ao3 site.

--- database/models.py
+++ database/models.py
@@ -11,6 +11,11 @@ from datetime import datetime
 
 Base = declarative_base()
 
+#######################################
+# Function: Fanfic(Base)
+# Description: Class function to create
+# the fields for the database.
+#######################################
 class Fanfic(Base):
     __tablename__ = 'fanfics'
 
@@ -24,10 +29,23 @@ class Fanfic(Base):
     last_checked = Column(DateTime, default=datetime.utcnow)
 
 
+#######################################
+# Function: get_engine()
+# Description: Function to create the 
+# database named fanfic_archive.db.
+#######################################
 def get_engine(db_url="sqlite:///database/fanfic_archive.db"):
     return create_engine(db_url)
 
 
+#######################################
+# Function: init_db()
+# Description: Function to init the 
+# database, which calls get_engine()
+# function. It uses Base module to
+# create the metadata with all the 
+# fields of the database.
+#######################################
 def init_db():
     engine= get_engine()
     Base.metadata.create_all(engine)
